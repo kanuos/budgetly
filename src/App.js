85b66196675429f2941c956afc96022e556d1d3a
kanuos <sounakmukherjee@ymail.com>
2020-11-03 18:18:18 -0500

    input group

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import InputGroup from './components/InputGroup/InputGroup';
+
 function App() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({
+      name, email, password
+    });
+  }
+
   return (
-    <div className="App">
+    <>
       <h1>Hello world</h1>
-    </div>
+      
+      <form onSubmit={handleSubmit}>
+        <InputGroup 
+          type = "text"
+          placeholder = "Your name"
+          getValue = {c => setName(() => c)}
+          />
+        <InputGroup 
+          type = "email"
+          placeholder = "Your email"
+          getValue = {c => setEmail(() => c)}
+          />
+        <InputGroup 
+          type = "password"
+          placeholder = "Your password"
+          getValue = {c => setPassword(() => c)}
+          />
+        <button>Submit</button>
+      </form>
+    </>
   );
 }
 
