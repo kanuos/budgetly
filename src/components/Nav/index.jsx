f2c9e658b3a9e16277a7cafc80e678eb4cdd768f
kanuos <sounakmukherjee@ymail.com>
2020-11-05 17:06:22 -0500

    navbar mobile update 1

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,37 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom'
 import Brand from './brand'
 import './index.css';
+import profile from '../../assets/face.svg';
+import stats from '../../assets/stats.svg';
+import calculate from '../../assets/calculate.svg';
+import exit from '../../assets/exit.svg';
 
 const Nav = () => {
+    const [navOpen, setNav] = useState(false);
+    function toggleNav() {
+        setNav(() => !navOpen);
+    }
 
-let user = [true, false][(Math.floor(Math.random() * 2))];
+    return (
+    <nav className="mobile-only">
+        <Brand />
+        <ul className="navbar-list">
+            {navOpen ?
+            <li className="navbar-link-item">
+                <Link to="/" className="navbar-link" onClick={toggleNav}>
+                    log out   
+                </Link>
+            </li>
+            :
 
-return (
-<nav className="mobile-only">
-    <Brand />
-    <ul className="navbar-list">
-        {user ?
-        <>
-        <li className="navbar-item">
-            <a href="#" className="navbar-link">
-                dashboard
-            </a>
-        </li>
-        <li className="navbar-link-item">
-            <a href="#" className="navbar-link">
-                log out   
-            </a>
-        </li>
-        </>
-        :
-
-        <li className="navbar-link-item">
-            <a href="#" className="navbar-link">
-                Try Demo    
-            </a>
-        </li>}
-    </ul>
-</nav>    
+            <li className="navbar-link-item">
+                <Link to="/" className="navbar-link" onClick={toggleNav}>
+                    Try Demo    
+                </Link>
+            </li>}
+        </ul>
+        <ul className={`navbar-list--alt ${navOpen ? "nav-open":"nav-close"}`}>
+            <li>
+                <Link to="/" className="nav-brand--alt">
+                    <Brand />
+                </Link>
+            </li>
+            <li>
+                <Link to="/"className="nav-link--alt" >
+                <img
+                    src={profile} 
+                    alt="profile"/>
+                    profile
+                </Link>
+            </li>
+            <li>
+                <Link to="/"className="nav-link--alt" >
+                <img
+                    src={stats} 
+                    alt="history"/>
+                    history
+                </Link>
+            </li>
+            <li>
+                <Link to="/" className="nav-link--alt">
+                <img
+                    src={calculate} 
+                    alt="calculator"/>
+                    calculator
+                </Link>
+            </li>
+            <li>
+                <Link to="/" className="nav-link--alt" onClick={toggleNav}>
+                <img
+                    src={exit} 
+                    alt="exit"/>
+                    sign out
+                </Link>
+            </li>
+        </ul> 
+    </nav>
 )}
 
 export default Nav;
