fadf2494cf4d839b989dcfd9ae75bd4baad11ef7
kanuos <sounakmukherjee@ymail.com>
2020-11-09 21:47:12 -0500

    placeholder image for landing

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -8,11 +8,38 @@ const index = () => {
         <>
         <header className="landing-header">
             <Brand />
-            <article className="web-only">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore at deserunt cupiditate commodi accusamus similique?
+            <section className="web-only landing-article-box">
+                <article className="landing-article">
+                    <h2>
+                        track your budget
+                    </h2>
+                    <img src="https://picsum.photos/536/354" alt=""/>
+                </article>
+                <article className="landing-article">
+                    <h2>
+                        plan your investments
+                    </h2>
+                    <img src="https://picsum.photos/seed/picsum/536/354" alt=""/>
+                </article>
+                <article className="landing-article">
+                    <h2>
+                        visualize your budget
+                    </h2>
+                    <img src="https://picsum.photos/id/237/536/354" alt=""/>
+                </article>
+            </section>
+            <article className="web-only demo-link-box">
+                <button className="demo-link">
+                    try demo
+                </button>
             </article>
             <div className="header-form-box">
                 <Account />
+                <article className="mobile-only demo-link-box">
+                    <button className="demo-link">
+                        try demo
+                    </button>
+                </article>
             </div>
         </header>
         <Footer />
